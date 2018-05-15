f980543d596edaa345ca278d915a107368f58b70
Roy Milner <roy@Roys-MacBook-Air.local>
2018-05-14 20:37:14 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html>
+	<head>
+		<?php include 'inc/head_common.php'; ?>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		
+		<article class="principal">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-xs-12">
+						<!--
+						<p align="left"><link ><img src="img/fondo.jpg" width="100%" alt=""></link></p>
+						-->
+					</div>
+
+				</div>
+			</div>
+		</article>
+		<!--
+		<hr noshade="noshade" />
+		<?php include 'inc/menu.php'; ?>
+		<hr noshade="noshade" />
+		-->
+
+		<section id="products">
+		
+				<!-- HELADERIA CHOCOCHIPS --> 
+				<div class="container" >
+					<div class="row" id="services">
+						<div class="col-xs-12">
+							<!--<h2>HELADERIA CHOCOCHIPS<h2> 	-->
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/fondo.jpg" width="100%" hight="250px"></link>
+							<br></br>
+							
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/1.jpg" width="275" ></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Galleta de Oreo</a></li>
+								<li><a href="#">
+									<span class="price"> S/1.50</span> 
+									<span class="texto"> Disponible 100 unidades</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/2.jpg" width="270"></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Galleta chocochips</a></li>
+								<li><a href="#">
+									<span class="price"> S/1.50</span> 
+									<span class="texto"> Disponible 100 unidades</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/5.jpg" width="260" height="250"></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Galleta de Chocolate</a></li>
+								<li><a href="#">
+									<span class="price"> S/7.50</span> 
+									<span class="texto"> Disponible 100 unidades</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/4.jpg" width="260" height="250"></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Galleta con 2 sabores de helado</a></li>
+								<li><a href="#">
+									<span class="price"> S/10.50</span> 
+									<span class="texto"> Disponible 100 unidades</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+		</section>
+
+		<hr noshade="noshade" />
+		<br>
+
+		<!-- FOOTER -->
+
+		<section id="table-of-contents">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-xs-4">
+						<h4 align="center">REDES SOCIALES</h4>
+						<ul>
+							<li><p align="center">facebook.com/heladeriachocochips</p></li>
+							<li><p align="center">Arequipa, Peru</p></li>
+						</ul>
+					</div>
+					<div class="col-xs-4">
+						<section id="agenda">
+							<h4 align="center">HORARIO DE ATENCION</h4>
+							<dl class="dl-horizontal">
+								<dt>Lunes</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Martes</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Miercoles</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Jueves</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Viernes</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Sabado</dt>
+								<dd>10:00 AM – 21:00 PM</dd>
+								<dt>Domingo</dt>
+								<dd>09:00 AM – 13:00 PM</dd>
+							</dl>
+						</section>
+					</div>
+
+					<div class="col-xs-4">
+						<h4 align="center">CONTÁCTANOS</h4>
+						<ul>
+							<li><p align="center">heladeriachocochips@gmail.com</p></li>
+							<li><p align="center">946727048</p></li>
+						</ul>
+
+					</div>
+
+				</div>
+			</div>
+		</section>
+		
+
+		<hr noshade="noshade" />
+
+		<?php include 'inc/footer.php'; ?>
+		<?php include 'inc/footer_common.php'; ?>
+	</body>
+</html>
